11cf72147668cdd20cf2de78c39ee27dc142cace
Mateusz Rokosa <238501@student.pwr.edu.pl>
2019-03-10 18:38:39 +0100

    Initial commit

---
+++ src/main/java/com/zpi/printerhub/PrinterHubApplication.java
@@ -0,0 +1,13 @@
+package com.zpi.printerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrinterHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zpi/printerhub/PrinterHubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zpi.printerhub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrinterHubApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
