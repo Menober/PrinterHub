e78e05b591433768c9c0621d3290f2f91ab7550c
Mateusz <mati.starczyk@gmail.com>
2019-03-30 11:02:46 +0100

    FDFDFDFDFDFD

--- src/main/java/com/zpi/printerhub/PrinterHubApplication.java
+++ src/main/java/com/zpi/printerhub/PrinterHubApplication.java
@@ -2,28 +2,12 @@ package com.zpi.printerhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 
 @SpringBootApplication
-public class PrinterHubApplication extends SpringBootServletInitializer {
+public class PrinterHubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PrinterHubApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(PrinterHubApplication.class);
-    }
 }
-
-//@SpringBootApplication
-//public class PrinterHubApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(PrinterHubApplication.class, args);
-//    }
-//
-//}
